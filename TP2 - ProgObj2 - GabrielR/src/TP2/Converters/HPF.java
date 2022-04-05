6abf3e541926b1e5e25229ec09c444d0e3b80a74
Bird <ninjabirdpepito@gmail.com>
2022-04-05 17:09:27 -0400

    i seem to have managed to put the ROT converter in the wrong class... again

--- TP2 - ProgObj2 - GabrielR/src/TP2/Converters/HPF.java
+++ TP2 - ProgObj2 - GabrielR/src/TP2/Converters/HPF.java
@@ -2,22 +2,4 @@ package TP2.Converters;
 
 public class HPF {
 
-    public String HPF (String receivedValue , int valueVariation){
-        char[] hexadecimals = {'A' , 'B' , 'C' , 'D' , 'E'};
-        int preCalc = Integer.parseInt(receivedValue);
-        int addValue = preCalc + valueVariation;
-        if (addValue >= 0 || addValue <= 15){
-            if (addValue < 10){
-                return String.valueOf(addValue);
-            }
-        } else {
-            int rotatedValue = addValue % 15;
-            if (rotatedValue < 10){
-                return String.valueOf(rotatedValue);
-            } else {
-                return String.valueOf(hexadecimals[rotatedValue - 10]);
-            }
-        }
-        return "";
-    }
 }
