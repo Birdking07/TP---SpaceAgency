d91663499806932976756735f13cdf096e83bc03
Bird <ninjabirdpepito@gmail.com>
2022-04-05 15:52:49 -0400

    HPF converter completed + SystemController added to commit as it was missing

--- TP2 - ProgObj2 - GabrielR/src/TP2/SystemController.java
+++ TP2 - ProgObj2 - GabrielR/src/TP2/SystemController.java
@@ -4,6 +4,13 @@ public class SystemController {
 
     public static void main(String[] Args){
 
-        
+        BigGenerator gg = new BigGenerator();
+
+    }
+    public void convert(){
+
+    }
+    public void analyse(){
+
     }
 }
