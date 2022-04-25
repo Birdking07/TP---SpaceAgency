dc8d770c58ca0e8dfca0f679b2aca911ea8c3251
Gabriel.R <ninjabirdpepito@gmail.com>
2022-04-24 23:34:45 -0400

    1D table complete -> GO RANT TO TEACHER
    Begin 2D table (hell) next

--- TP2 - ProgObj2 - GabrielR/src/TP2/Analysers/OneDim.java
+++ TP2 - ProgObj2 - GabrielR/src/TP2/Analysers/OneDim.java
@@ -24,17 +24,37 @@ public class OneDim extends AnalyserFormat{
     public boolean OneSequenceCount(String chain, String sequence, int objective) {
         int counter = 0;
 
-            while (chain.contains(sequence) && counter < objective){
-                int position = chain.indexOf(sequence);
-                System.out.println(position);
-               chain = chain.substring(position);
-               counter++;
-            }
-            return objective == counter;
+             chain = chain.replaceAll(sequence , "X");
+               for (int i = 0 ; i < chain.length() ; i++){
+                   if (chain.charAt(i) == 'X'){
+                       counter++;
+                   }
+               }
+            return counter == objective;
+               //correctly does it's job but values given are incorrect?
     }
 
 
     public boolean AllSequencesCount(String chain, ArrayList<String> sequenceList, int objective) {
-        return false;
+        int[] counter = new int[3];
+        String originalValue = chain;
+        int objectiveCounter = 0;
+     for (int i = 0 ; i < sequenceList.size() ; i++){
+         chain = originalValue;
+         chain = chain.replaceAll(sequenceList.get(i) , "X");
+         for (int a = 0 ; a < chain.length() ; a++){
+             if (chain.charAt(a) == 'X'){
+                 counter[i]++;
+             }
+         }
+     }
+     for (int i = 0 ; i < sequenceList.size() ; i++){
+         if (counter[i] == objective){
+             objectiveCounter++;
+         } else {
+             return false;
+         }
+     }
+     return objectiveCounter == sequenceList.size();
     }
 }
